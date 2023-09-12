2c2c84dc95de753d6db1a22fe6082c6a5345cdbb
Jeongsuwon <129022812+Jeongsuwon@users.noreply.github.com>
2023-09-12 23:23:17 +0900

    로그인 Controller구현

--- crud/src/main/java/com/crud/CrudApplication.java
+++ crud/src/main/java/com/crud/CrudApplication.java
@@ -2,7 +2,6 @@ package com.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CrudApplication {
@@ -10,8 +9,5 @@ public class CrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
-	@GetMapping("/")
-	public String login() {
-		return "login";
-	}
+
 }
